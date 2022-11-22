37372d9c81bb44cc5d5794b19b9c41a7ddd41371
anaboru <slysetrrr71969@gmail.com>
2022-11-23 01:15:25 +0300

    10

--- src/main/java/hiber/dao/UserDaoImp.java
+++ src/main/java/hiber/dao/UserDaoImp.java
@@ -25,5 +25,12 @@ public class UserDaoImp implements UserDao {
       TypedQuery<User> query=sessionFactory.getCurrentSession().createQuery("from User");
       return query.getResultList();
    }
+   public User getUserByCar(String model, int series) {
+      TypedQuery<User> query = sessionFactory.getCurrentSession()
+              .createQuery("from User where car.model=:model and car.series=:series")
+              .setParameter("model", model)
+              .setParameter("series", series);
+      return query.getSingleResult();
+   }
 
 }
--- src/main/java/hiber/model/User.java
+++ src/main/java/hiber/model/User.java
@@ -19,14 +19,30 @@ public class User {
    @Column(name = "email")
    private String email;
 
-   public User() {}
+   @OneToOne(cascade = CascadeType.ALL)
+   @JoinColumn(name="car_id")
+   private Car car;
+
+   public User() {
+
+   }
    
    public User(String firstName, String lastName, String email) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
    }
+   public User(String firstName, String lastName, String email, Car car) {
+      this(firstName, lastName, email);
+      this.car = car;
+   }
 
+   public void setCar(Car car) {
+      this.car = car;
+   }
+   public Car getCar() {
+      return car;
+   }
    public Long getId() {
       return id;
    }
@@ -57,5 +73,16 @@ public class User {
 
    public void setEmail(String email) {
       this.email = email;
+   }
+
+   @Override
+   public String toString() {
+      return "User{" +
+              "id=" + id +
+              ", firstName='" + firstName + '\'' +
+              ", lastName='" + lastName + '\'' +
+              ", email='" + email + '\'' +
+              ", car=" + car +
+              '}';
    }
 }
